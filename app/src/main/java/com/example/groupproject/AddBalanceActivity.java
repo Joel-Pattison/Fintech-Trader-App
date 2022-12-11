98e5acdfd9b728667ae0179f3a0d0be7c82fae8a
Joel Pattison <113473745+Joel-Pattison@users.noreply.github.com>
2022-12-11 14:06:44 +0000

    created basic functionality for adding balance to account

--- app/src/main/java/com/example/groupproject/AddBalanceActivity.java
+++ app/src/main/java/com/example/groupproject/AddBalanceActivity.java
@@ -1,11 +1,21 @@
 package com.example.groupproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.paypal.checkout.PayPalCheckout;
 import com.paypal.checkout.approve.Approval;
 import com.paypal.checkout.approve.OnApprove;
@@ -33,22 +43,46 @@ import java.util.ArrayList;
 public class AddBalanceActivity extends AppCompatActivity {
 
     private static final String YOUR_CLIENT_ID = "AYFpCIrqcwNop2ULS5_Cwp9kSyi8pIsOhThRB521awH4d-xYDvnkRsqvYYccirxSQPk1KG-jvBiZbnYy";
-    PaymentButtonContainer payPalButton;
+/*    PaymentButtonContainer payPalButton;*/
+    Button btnAddFunds;
+    PayPalCheckout payPalCheckout;
+    FirebaseFirestore db;
+    private String userID;
+    User curUserProfile;
+    EditText txtTopupAmount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_balance);
 
-        PayPalCheckout.setConfig(new CheckoutConfig(
+        btnAddFunds = findViewById(R.id.btnAddFunds);
+        txtTopupAmount = findViewById(R.id.txtTopupAmount);
+
+        getCurUserProfile();
+
+        btnAddFunds.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (txtTopupAmount.getText().toString().isEmpty()) {
+                    txtTopupAmount.setError("Please enter a valid amount");
+                    txtTopupAmount.requestFocus();
+                } else{
+                    addBalance();
+                }
+            }
+        });
+
+/*        PayPalCheckout.setConfig(new CheckoutConfig(
                 getApplication(),
                 YOUR_CLIENT_ID,
                 Environment.SANDBOX,
                 CurrencyCode.USD,
                 UserAction.PAY_NOW
-        ));
+        ));*/
 
-        payPalButton = findViewById(R.id.paymentButtonContainer);
+/*        payPalButton = findViewById(R.id.paymentButtonContainer);
 
         payPalButton.setup(
                 new CreateOrder() {
@@ -86,6 +120,46 @@ public class AddBalanceActivity extends AppCompatActivity {
                         });
                     }
                 }
-        );
+        );*/
+    }
+
+    private void addBalance() {
+        String balanceToAdd = txtTopupAmount.getText().toString();
+        double balanceToAddDouble = Double.parseDouble(balanceToAdd);
+        double curBalance = Double.parseDouble(curUserProfile.balance.get("euroBalance"));
+        double newBalance = curBalance + balanceToAddDouble;
+        curUserProfile.balance.put("euroBalance", String.valueOf(newBalance));
+        setCurUserProfile();
+    }
+
+    private void getCurUserProfile(){
+        db = FirebaseFirestore.getInstance();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        userID = user.getUid();
+
+        db.collection("users").document(userID)
+                .get().addOnCompleteListener(task -> {
+                    if(task.isSuccessful() && task.getResult() != null){
+                        curUserProfile = task.getResult().toObject(User.class);
+                    }else{
+                        Toast.makeText(AddBalanceActivity.this, "Something went wrong when trying to get your data.", Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
+
+    private void setCurUserProfile(){
+        db.collection("users").document(userID).set(curUserProfile).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(AddBalanceActivity.this, "Successfully added balance", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(AddBalanceActivity.this, "Failed to add balance", Toast.LENGTH_SHORT).show();
+                }
+                Intent send = new Intent(AddBalanceActivity.this, HomeActivity.class);
+                startActivity(send);
+            }
+        });
     }
 }
